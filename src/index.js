0bd099a605fc78ebe9baab4a0c7209c34a87b1f6
SKhrebtan <shrebtan@gmail.com>
2023-07-09 13:13:51 +0300

    add name and description

--- src/index.js
+++ src/index.js
@@ -5,30 +5,35 @@ axios.defaults.headers.common["x-api-key"] = "live_2jIJfwcZBQTHZg4p85o314KTbd5zR
 const select = document.querySelector('.breed-select');
 const div = document.querySelector('.cat-info');
 fetch('https://api.thecatapi.com/v1/breeds/')
-    .then(response => { return response.json() })
-    .then(function fetchBreeds(response) {
-                return response.map((t) => {
-                // console.log(t.description)
-              return `<option value="${t.id}">${t.name}</option>`;              
-                }).join('')
-    })
-  .then(up => select.innerHTML = up)
-  .then(cat => {
-    console.log(cat);
+  .then(response => { return response.json() })
+  .then(response => {
+    const markup = response.map((t) => {
+        console.log(t);
+        return `<option value="${t.id}" descr="${t.description}" name="${t.name}">${t.name}</option>`;
+      }).join('');
+    select.innerHTML = markup;
+          
     select.addEventListener('change', onSelectInput);
     function onSelectInput(e) {
-      console.log(cat)
-      fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${select.value}`)
-        .then(response => { return response.json() }).
-        then(t => {
-          div.innerHTML = '';
-          console.log(t[0]);
-           const img = document.createElement('img');
-  img.setAttribute("src", `${t[0].url}`);
-          img.setAttribute("alt", "Amazing nature");
-          img.setAttribute("width", "480px");
-                   div.appendChild(img);
-        })
+      const option = select.querySelector(`option[value="${select.value}"]`);
+            fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${select.value}`)
+           .then(response => { return response.json() })
+           .then(t => {
+             console.log(response);
+                     div.innerHTML = '';
+          const img = document.createElement('img');
+             const descr = document.createElement('p');
+             const name = document.createElement('p');
+             descr.textContent = option.getAttribute('descr');
+             name.textContent = option.getAttribute('name');
+            img.setAttribute("src", `${t[0].url}`);
+            img.setAttribute("alt", "Amazing nature");
+            img.setAttribute("width", "480px");
+             div.appendChild(img);
+             div.appendChild(name);
+             div.appendChild(descr);
+               })
     }
-  })
-
+    }
+      );
+ 
