7623f9bedd13bf50411530b6d4c3cc29d68ca203
SKhrebtan <shrebtan@gmail.com>
2023-07-09 16:17:46 +0300

    first version, vaery bad

--- src/index.js
+++ src/index.js
@@ -1,52 +1,70 @@
 import axios from "axios";
+import { fetchBreeds } from './cat-api';
+import { fetchCatByBreed } from './fetch-cat-by-breed';
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select'
+
+// new SlimSelect({
+//   select: '#selectElement',
+// })
 
 axios.defaults.headers.common["x-api-key"] = "live_2jIJfwcZBQTHZg4p85o314KTbd5zRvyg8dxZzmKirNbBbL8mHf7vy0nplnD7JLOE";
 
 const select = document.querySelector('.breed-select');
 const div = document.querySelector('.cat-info');
+const pLoader = document.querySelector('.loader');
 
+onPageLoad();
 
-
+function onPageLoad() {
+select.style.display = 'none';
+div.style.display = 'none';
+pLoader.style.display = 'block';
+};
+ 
 fetchBreeds()
   .then(response => {
+    div.style.display = 'none';
+    pLoader.style.display = 'none';
     renderCatCard(response);
-            
+    
     select.addEventListener('change', onSelectInput);
     function onSelectInput(e) {
-      const option = select.querySelector(`option[value="${select.value}"]`);
-            fetchCatByBreed(select.value)
-           .then(t => {
-             div.innerHTML = '';
-             const img = document.createElement('img');
+      div.style.display = 'none';
+      pLoader.style.display = 'block';
+      fetchCatByBreed(select.value)
+        .then(catCardCreate);
+       }
+    
+  }
+).catch(() => {Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')})
+  .finally(() => { pLoader.style.display = 'none'; }); 
+
+
+
+function renderCatCard(response) {
+  const markup = response.map((t) => {
+        return `<option value="${t.id}" descr="${t.description}" name="${t.name}">${t.name}</option>`;
+  }).join('');
+  select.style.display = 'block';
+    select.innerHTML = markup;
+}
+
+function catCardCreate(t) {
+  const option = select.querySelector(`option[value="${select.value}"]`);
+  div.innerHTML = '';
+  pLoader.style.display = 'none';
+   div.style.display = 'block';
+            const img = document.createElement('img');
              const descr = document.createElement('p');
-             const name = document.createElement('p');
+             const name = document.createElement('h1');
              descr.textContent = option.getAttribute('descr');
-             name.textContent = option.getAttribute('name');
-             img.setAttribute("src", `${t[0].url}`);
+  name.textContent = option.getAttribute('name');
+  descr.style.width = '480px';
+               img.setAttribute("src", `${t[0].url}`);
              img.setAttribute("alt", "Amazing nature");
              img.setAttribute("width", "480px");
              div.appendChild(img);
              div.appendChild(name);
              div.appendChild(descr);
-               })
-    }
-    }
-      );
- 
-function fetchBreeds() {
-  return fetch('https://api.thecatapi.com/v1/breeds/')
-  .then(response => { return response.json() })
-}
-
-function renderCatCard(response) {
-  const markup = response.map((t) => {
-        console.log(t);
-        return `<option value="${t.id}" descr="${t.description}" name="${t.name}">${t.name}</option>`;
-      }).join('');
-    select.innerHTML = markup;
-}
-      
-function fetchCatByBreed(value) {
-  return    fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${select.value}`)
-           .then(response => { return response.json() })
 }
