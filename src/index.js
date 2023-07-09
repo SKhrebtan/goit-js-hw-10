05189e9f76615a689c9cacb5743d006d07567eed
SKhrebtan <shrebtan@gmail.com>
2023-07-09 11:29:22 +0300

    add img

--- src/index.js
+++ src/index.js
@@ -3,28 +3,32 @@ import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = "live_2jIJfwcZBQTHZg4p85o314KTbd5zRvyg8dxZzmKirNbBbL8mHf7vy0nplnD7JLOE";
 
 const select = document.querySelector('.breed-select');
-console.log(select)
+const div = document.querySelector('.cat-info');
 fetch('https://api.thecatapi.com/v1/breeds/')
     .then(response => { return response.json() })
-    .then(function Markup(response) {
-        let listMarkup
-                return listMarkup = response.map((t) => {
-                console.log(t.id)
+    .then(function fetchBreeds(response) {
+                return response.map((t) => {
+                // console.log(t.description)
               return `<option value="${t.id}">${t.name}</option>`;              
-                        }).join('')}).then(up => select.innerHTML = up  )
+                }).join('')
+    })
+  .then(up => select.innerHTML = up)
+  .then(cat => {
+    console.log(cat);
+    select.addEventListener('change', onSelectInput);
+    function onSelectInput(e) {
+      console.log(cat)
+      fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${select.value}`)
+        .then(response => { return response.json() }).
+        then(t => {
+          div.innerHTML = '';
+          console.log(t[0]);
+           const img = document.createElement('img');
+  img.setAttribute("src", `${t[0].url}`);
+          img.setAttribute("alt", "Amazing nature");
+          img.setAttribute("width", "480px");
+                   div.appendChild(img);
+        })
+    }
+  })
 
-// select.innerHTML = listMarkup  
-//    function Markup(response) {
-//                     const listMarkup = response.map((t) => {
-//                 console.log(t.id)
-//               return `<option value="${t.id}">${t.name}</option>`;              
-//                         }).join('')}
-// function Markup(response) {
-//                   return response.map((t) => {
-//                 console.log(t.id);
-//                 `<option value="${t.id}">${t.name}</option>`;
-//             })
-//                 .join('');
-//         },
-//         select.insertAdjacentHTML('beforeend', response)
-//  fetch(`https://api.thecatapi.com/v1/images/${t.reference_image_id}`).then(img => { return img.json() }).then(t => console.log(t.url));
