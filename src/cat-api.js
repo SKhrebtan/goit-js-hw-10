7db312c87d75ce5e2d556787623b4abe1cfc6116
SKhrebtan <shrebtan@gmail.com>
2023-07-10 18:13:58 +0300

    key fix

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,4 +1,16 @@
 export const fetchBreeds = function () {
-   return fetch('https://api.thecatapi.com/v1/breeds/')
-  .then(response => { return response.json() })
-}
+  return fetch('https://api.thecatapi.com/v1/breeds/')
+    .then(response => { return response.json() })
+    .catch(error => {
+      console.log(error);
+      Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')
+    })
+};
+
+export const fetchCatByBreed = function (id) {
+  return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}&api_key=live_2jIJfwcZBQTHZg4p85o314KTbd5zRvyg8dxZzmKirNbBbL8mHf7vy0nplnD7JLOE`)
+    .then(response => { return response.json() })
+    .catch(error => {
+      console.log(error);
+    });
+};
--- src/index.js
+++ src/index.js
@@ -1,70 +1,91 @@
 import axios from "axios";
-import { fetchBreeds } from './cat-api';
-import { fetchCatByBreed } from './fetch-cat-by-breed';
+import { fetchBreeds, fetchCatByBreed} from './cat-api';
 import Notiflix from 'notiflix';
-import SlimSelect from 'slim-select'
-
-// new SlimSelect({
-//   select: '#selectElement',
-// })
+import SlimSelect from 'slim-select';
 
 axios.defaults.headers.common["x-api-key"] = "live_2jIJfwcZBQTHZg4p85o314KTbd5zRvyg8dxZzmKirNbBbL8mHf7vy0nplnD7JLOE";
 
 const select = document.querySelector('.breed-select');
-const div = document.querySelector('.cat-info');
+const catInfo = document.querySelector('.cat-info');
 const pLoader = document.querySelector('.loader');
 
 onPageLoad();
 
 function onPageLoad() {
 select.style.display = 'none';
-div.style.display = 'none';
+catInfo.style.display = 'none';
 pLoader.style.display = 'block';
 };
  
 fetchBreeds()
   .then(response => {
-    div.style.display = 'none';
+    catInfo.style.display = 'none';
     pLoader.style.display = 'none';
     renderCatCard(response);
-    
-    select.addEventListener('change', onSelectInput);
+    }
+).catch((error) => {
+  console.log(error);
+  Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!');
+})
+  .finally(() => { pLoader.style.display = 'none'; }); 
+
+select.addEventListener('change', onSelectInput);
     function onSelectInput(e) {
-      div.style.display = 'none';
+      catInfo.style.display = 'none';
       pLoader.style.display = 'block';
       fetchCatByBreed(select.value)
         .then(catCardCreate);
        }
-    
-  }
-).catch(() => {Notiflix.Notify.failure('Oops! Something went wrong! Try reloading the page!')})
-  .finally(() => { pLoader.style.display = 'none'; }); 
-
-
 
 function renderCatCard(response) {
-  const markup = response.map((t) => {
-        return `<option value="${t.id}" descr="${t.description}" name="${t.name}">${t.name}</option>`;
+  const markup = response.map((cat) => {
+    return `<option value="${cat.id}" descr="${cat.description}" name="${cat.name}" temp="${cat.temperament}">${cat.name}</option>`;
   }).join('');
   select.style.display = 'block';
-    select.innerHTML = markup;
+  select.innerHTML = markup;
+
+  // new SlimSelect({
+  //   select: '#selectElement',
+  //  })
+}
+    function catCardCreate(cat) {
+            
+      const catName = cat[0].breeds[0].name;
+      const catImg = cat[0].url;
+      const catTemperamnet = cat[0].breeds[0].temperament;
+      const catDescription = cat[0].breeds[0].description;
+     
+      catInfo.innerHTML = '';
+     pLoader.style.display = 'none';
+      catInfo.style.display = 'block';
+      
+      const markup = `
+      <h1>${catName}</h1>
+      <img src="${catImg}" alt="${catName}" width="640">
+      <p class="cat-descr">${catDescription}</p>
+      <p><span class="card-span">Temperament: </span>${catTemperamnet}</p>`;
+      catInfo.innerHTML = markup;
+        
 }
 
-function catCardCreate(t) {
-  const option = select.querySelector(`option[value="${select.value}"]`);
-  div.innerHTML = '';
-  pLoader.style.display = 'none';
-   div.style.display = 'block';
-            const img = document.createElement('img');
-             const descr = document.createElement('p');
-             const name = document.createElement('h1');
-             descr.textContent = option.getAttribute('descr');
-  name.textContent = option.getAttribute('name');
-  descr.style.width = '480px';
-               img.setAttribute("src", `${t[0].url}`);
-             img.setAttribute("alt", "Amazing nature");
-             img.setAttribute("width", "480px");
-             div.appendChild(img);
-             div.appendChild(name);
-             div.appendChild(descr);
-}
+
+// function catCardCreate(t) {
+//   const option = select.querySelector(`option[value="${select.value}"]`);
+//   div.innerHTML = '';
+//    pLoader.style.display = 'none';
+//    div.style.display = 'block';
+//             const img = document.createElement('img');
+//              const descr = document.createElement('p');
+//   const name = document.createElement('h1');
+//   const temp = document.createElement('p');
+//              descr.textContent = option.getAttribute('descr');
+//   name.textContent = option.getAttribute('name');
+//   temp.textContent = `Temperament: ${option.getAttribute('temp')}`;
+//   descr.style.width = '480px';
+//                img.setAttribute("src", `${t[0].url}`);
+//              img.setAttribute("alt", "Amazing nature");
+//              img.setAttribute("width", "480px");
+//              div.appendChild(img);
+//              div.appendChild(name);
+//   div.appendChild(descr);
+//   div.appendChild(temp);}
