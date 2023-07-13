a56c6b03b0ec011d41dd3b988181db9a73ddcc3c
SKhrebtan <shrebtan@gmail.com>
2023-07-13 08:47:34 +0300

    add response status check

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,11 +1,20 @@
 export const fetchBreeds = function () {
   return fetch('https://api.thecatapi.com/v1/breeds/')
-    .then(response => { return response.json() });   
+    .then(response => { 
+      if (response.status !== 200) {
+        throw new Error(response.status)
+      };
+      return response.json() });   
 };
 
 export const fetchCatByBreed = function (id) {
   return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}&api_key=live_2jIJfwcZBQTHZg4p85o314KTbd5zRvyg8dxZzmKirNbBbL8mHf7vy0nplnD7JLOE`)
-    .then(response => { return response.json() });
+    .then(response => {
+     if (response.status !== 200) {
+        throw new Error(response.status)
+      };
+      return response.json()
+    });
 };
 
 // .catch(error => {
